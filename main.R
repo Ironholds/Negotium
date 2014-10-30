cc3be6c671bc8466abdc0fe5ddfdbcb727332b6a
ironholds <ironholds@gmail.com>
2014-10-30 13:07:47 -0400

    reorg

--- main.R
+++ main.R
@@ -12,6 +12,7 @@ main <- function(){
   
   #Convert timestamps
   data$timestamp <- as.numeric(log_strptime(data$timestamp))
+  data <- data[!is.na(data),]
   
   #Split
   split_data <- keysplit(obj = data, key_col = "uuid")
@@ -22,7 +23,29 @@ main <- function(){
                                 
                                 #Split again and lapply
                                 interim_results <- lapply(X = keysplit(obj = x, key_col = "uuid", pieces = length(unique(x$uuid))),
-                                                          FUN = result_generator)
+                                                          FUN = function(events){
+                                                            
+                                                            #If there's only one event, this is easy - just return with the default
+                                                            if(nrow(events) == 1){
+                                                              
+                                                              return(list(pages = 1,
+                                                                          sessions = 1,
+                                                                          session_length = 430)
+                                                              )
+                                                              
+                                                            }
+                                                            
+                                                            #Otherwise, compute intertimes
+                                                            intertime_vals <- intertimes(timestamps = events$timestamp)
+                                                            
+                                                            #And then generate, in sequence, and return...
+                                                            return(list(pages = session_pages(intertime_vals), #Number of pages in the session(s)
+                                                                        sessions = session_count(intertime_vals), #Number of sessions in the series of events
+                                                                        session_length = session_length(intertime_vals) #Length of the session(s)
+                                                            )
+                                                            )
+                                                            
+                                                          })
                                 
                                 return(interim_results)
                                 
