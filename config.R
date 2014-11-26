6bf53796afc54b73b416f97e5fb292427cade656
ironholds <ironholds@gmail.com>
2014-11-26 14:00:06 -0500

    add parallel as a dependency, change file to dir, remove default runlength.

--- config.R
+++ config.R
@@ -1,12 +1,10 @@
 #Libraries
 library(WMUtils) #Internal utilities. For great justice and C++!
 library(lubridate) #Timestamp handling.
+library(parallel) #Parallelisation
 
 #Options
 options(scipen = 500, #Scientific notation is silly
-        q = "no" #So is saving .RDatas
-        )
+        q = "no" #So is saving .RDatas)
         
-#Config variables
-DEFAULT_RUN_LENGTH = 7 #How many days to run over
-SAVE_FILE = "/a/aggregate-datasets/session_data.tsv" #Where to save the results
+save_dir = "/a/aggregate-datasets/apps/" #Where to save the results
